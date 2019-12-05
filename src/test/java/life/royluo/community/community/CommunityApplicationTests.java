ab40c006f382ae8bbecc8b75c25018baeea3e608
Roy5700xt <937139895@qq.com>
2019-12-05 20:07:01 +0800

    init repo

---
+++ src/test/java/life/royluo/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.royluo.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
