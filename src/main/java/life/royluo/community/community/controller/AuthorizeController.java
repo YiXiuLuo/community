ff71cc56b729a1f4072970eaab9b37a0316b379a
YiXiuLuo <937139895@qq.com>
2019-12-14 20:38:16 +0800

    extract github config from code to properties

--- src/main/java/life/royluo/community/community/controller/AuthorizeController.java
+++ src/main/java/life/royluo/community/community/controller/AuthorizeController.java
@@ -1,8 +1,10 @@
 package life.royluo.community.community.controller;
 
 import life.royluo.community.community.dto.AccessTokenDTO;
+import life.royluo.community.community.dto.GithubUser;
 import life.royluo.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,17 +14,28 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8881/callback");
-        accessTokenDTO.setClient_id("2dbec06cb53028c46cc9");
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_secret("79c8aaa913ba3e2e97084384810f4672a9115339");
+        accessTokenDTO.setClient_secret(clientSecret);
 
-        githubProvider.getAccessToken(accessTokenDTO);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
 
         return "index";
     }
--- src/main/java/life/royluo/community/community/provider/GithubProvider.java
+++ src/main/java/life/royluo/community/community/provider/GithubProvider.java
@@ -21,25 +21,28 @@ public class GithubProvider {
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
-            String s = response.body().string();
-            System.out.println("1111111111111"+s+"1111111111111");
-            return s;
-        } catch (IOException e) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            System.out.println(string);
+            System.out.println(token);
+            return token;
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return null;
     }
 
-    private GithubUser getUser(String accessToken){
+    public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url("https://api.github.com/user?access_token="+accessToken)
                 .build();
         try {
             Response response = client.newCall(request).execute();
-            String s = response.body().string();
-            GithubUser githubUser = JSON.parseObject(s, GithubUser.class);
+            String string = response.body().string();
+            System.out.println(string);
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
             return githubUser;
         } catch (IOException e) {
 
